6cf616399511639bd4ee6836c5a4221a130e7154
Jacob Glasdam <jacob@medware.dk>
2019-06-02 16:26:12 +0200

    Viser klasse

--- urscript.js
+++ urscript.js
@@ -3,3 +3,21 @@ var ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
+
+
+
+class Viser {
+  constructor(length, width, center_x, center_y) {
+    this.length = length;
+    this.width = width;
+    this.center_x = center_x;
+    this.center_y = center_y;
+  }
+
+  draw(context){
+    context.moveTo(center_x, center_y);
+    context.lineTo(center_x, length);
+    context.stroke();
+  }
+
+}
